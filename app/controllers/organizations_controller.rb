cf6c533d71a67012e088f88b3338a3a8f358068e
Aaron Xu <aaron_rg_xu@hotmail.com>
2016-11-16 16:37:27 -0800

    update

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,7 @@
 class OrganizationsController < ApplicationController
 
 	def index
-		@organizations = Organziations.all
+		@organizations = Organization.all
 	end
 
 	def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'organizations#index'
   get "/organizations", to: "organizations#index"
   get "/organizations/create", to: "organizations#create"
 end
