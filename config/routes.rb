59e16fc3f15d414aafecb23fe326fe83154ca565
Brenda Zhang <brenda.zhang2@gmail.com>
2016-11-16 19:48:29 -0800

    Minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   get "/organization", to: "organizations#index"
   get '/organization/new', to: 'organizations#new'
   post '/organization/create', to: 'organizations#create'
+
   get '/committee_member/new', to: 'committee_member#new'
   get '/committee_member/create', to: 'committee_member#create'
   post '/committee_member/create', to: 'committee_member#create'
+
   get '/officer/new', to: 'officers#new'
   post '/officer/create', to: 'officers#create'
+  
   patch '/delete', to: 'committee_member#delete'
 end
